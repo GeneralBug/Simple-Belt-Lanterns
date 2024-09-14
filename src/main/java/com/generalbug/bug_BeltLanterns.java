8659c3438a7d15f311bdf95c6461dff011974590
Bug <brumbus32@protonmail.com>
2024-09-14 18:45:14 +1000

    Set up project and converted datapack version into mod

--- src/main/java/com/generalbug/bug_BeltLanterns.java
+++ src/main/java/com/generalbug/bug_BeltLanterns.java
@@ -1,13 +1,12 @@
-package com.example;
+package com.generalbug;
 
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ExampleMod implements ModInitializer {
-	public static final String MOD_ID = "modid";
-
+public class bug_BeltLanterns implements ModInitializer {
+	public static final String MOD_ID = "bug_beltlanterns";
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
